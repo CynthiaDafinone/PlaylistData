00958936f44adeb500d31897cda2f9e6e30ff5d3
CynthiaDafinone <c.dafinone@gmail.com>
2018-08-15 14:47:52 +0100

    added show playlist file and installed spotipy

---
+++ showPlaylist.py
@@ -0,0 +1,14 @@
+import spotipy 
+
+scope = 'user-library-read'
+
+username = 
+
+token = util.prompt_for_user_token(username, scope)
+
+#util.prompt_for_user_token(username,scope,client_id='1110922843',client_secret='your-app-redirect-url',redirect_uri='your-app-redirect-url')
+
+#davids ID 	1110922843
+#my username  https://open.spotify.com/user/21qshqo4beqqk7bzu7gfnctqy?si=0ZRnGWxjTLmUwUPAYm_zsQ
+#client ID 	  spotify:user:21qshqo4beqqk7bzu7gfnctqy
+ 
