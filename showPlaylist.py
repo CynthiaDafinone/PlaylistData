467e6659d853959babacdc58c23703141cd8bb81
CynthiaDafinone <c.dafinone@gmail.com>
2018-09-11 14:31:49 +0100

    Got all metadata other than tracklength and genre

--- showPlaylist.py
+++ showPlaylist.py
@@ -4,10 +4,29 @@ import spotipy
 import spotipy.util as util
 
 
-def setcredentials(username):
+
+
+def getUsername(sp,uid):
+	#gets the usersname from a userID
+	uno = uid2uno(uid)
+	results = sp.user(uno)
+	print(results['display_name'])
+
+
+def uid2uno(uid):
+	#converts from a userID to a usernumber 
+	uno = uid.replace('spotify:user:','')
+	return uno 
+
+def tid2tno(tid):
+	#coverts from a trackID to a tracknumber
+	#tno = tid.replace('spotify:track:')
 	pass
 
-def showplaylistdata():
+
+def getGenre(tid):
+	#uses search function to return genre 
+	#tno = tid2tno(tid)
 	pass
 
 def mytracks(sp):
@@ -21,17 +40,53 @@ def myplaylists(sp):
 	for item in results['items']:
 		print(item)
 
+
+
+
 def officeplaylist(sp):
-	results = sp.user_playlist('1110922843','3gftLZFZYjMYszOlsQIqeA',limit=600)
-	#user_playlist_tracks()
-	#for item in results['items']:
-	#print(item)
-	print(results)
+	#results = sp.user_playlist('1110922843','3gftLZFZYjMYszOlsQIqeA',limit=600)
+	offs = 0 
+	for i in range(6):
+		
+		playlist = sp.user_playlist_tracks('1110922843','3gftLZFZYjMYszOlsQIqeA',fields=None, limit=100, offset=offs, market=None)
+	#fields=None, limit=50, offset=0, market=None)
+		offs = offs +100
+		for item in playlist['items']:
+			#print(item)
+			 #date and time added 
+			dateAdded = item['added_at']
+			print(dateAdded)
+
+			#uri of username -added by
+			username = getUsername(sp,item['added_by']['uri'])
+			print(username)
+
+		
+			#gets the name of each track 
+			trackName = item['track']['name']
+			print(trackName)
+
+			#uri of track for genre search - for genre 
+			trackURI = item['track']['uri'] 
+			print(trackURI)
+			#genre = getGenre (trackURI)
+
+			#gets the name of each album 
+			albumName = item['track']['album']['name']
+			print(albumName)
+			
+			#gets the name of each artist
+			artist = item['track']['album']['artists'][0]['name'] 
+			print(artist) 
+			
+	#print(results)
+
+	
 
 
 
 if __name__ == '__main__':
-
+	#spotify:user:1168610742
 	#prompt the user for their token to authenticate
 	username = "21qshqo4beqqk7bzu7gfnctqy?si=0ZRnGWxjTLmUwUPAYm_zsQ"
 	scope = 'user-library-read'
@@ -51,7 +106,22 @@ if __name__ == '__main__':
 		#spotify:user:1110922843:playlist:3gftLZFZYjMYszOlsQIqeA
 		#fields=None
 		officeplaylist(sp)
+		getUsername(sp,'1121686703')
 		
 	else:
 		print("Can't get token for", username)
 
+#--------------
+#Paramters needed
+
+#date added 
+#name of user who added song 
+#song name 
+#album name 
+#artist
+#genre * 
+#tracklength*
+
+#*metadata can be reached using the search function 
+
+#pip install --upgrade google-api-python-client oauth2client
